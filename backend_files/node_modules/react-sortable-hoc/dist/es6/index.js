6d1eae9adbfe9fb3453a03ec1f071f30ae106294
Benjamin Taubenblatt <BenjaminMT64@gmail.com>
2017-01-02 17:38:58 -0500

    fixed bug in register page

--- backend_files/node_modules/react-sortable-hoc/dist/es6/index.js
+++
@@ -1,8 +0,0 @@
-import _SortableContainer from './SortableContainer';
-export { _SortableContainer as SortableContainer };
-import _SortableElement from './SortableElement';
-export { _SortableElement as SortableElement };
-import _SortableHandle from './SortableHandle';
-export { _SortableHandle as SortableHandle };
-
-export { arrayMove } from './utils';
